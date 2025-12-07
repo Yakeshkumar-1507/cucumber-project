1a516adc7039ad262a4100cb042496bc918aef47
Yakeshkumar-1507 <yakeshsiva1507@outlook.com>
2025-12-07 17:53:42 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/FeatureFile/AdactinLogin.feature.feature",
+@CucumberOptions(features = "src/test/resources/FeatureFile/AdactinLogin.feature",
         glue = "steps", tags = "@smoke"
 //        plugin = {
 //                "pretty",
